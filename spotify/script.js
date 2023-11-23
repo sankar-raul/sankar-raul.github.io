aa4933f278ba8f48334b4edf5400737957f04926
sankar-coding <raulsankar99@gmail.com>
2023-11-23 11:11:13 +0530

    test

--- spotify/script.js
+++ spotify/script.js
@@ -1,10 +1,27 @@
-export function Get() {
-        const got = require('got');
+// script.js
 
-got("https://www.google.com/index.html").then(result => {
-    alert(result);
-}).catch(err => {
-    alert(err);
-});
+const got = require('got');
 
+function getSpotifyTrackInfo() {
+    // Replace '5BKKKNwrebowaMoUdBaNby' with the actual track ID
+    var trackId = '5BKKKNwrebowaMoUdBaNby';
+
+    // Spotify API endpoint for retrieving track information
+    var apiUrl = 'https://api.spotify.com/v1/tracks/' + trackId;
+
+    got(apiUrl)
+        .then(response => {
+            // Assuming got returns a response object with data
+            var data = response.body;
+            
+            // Handle the retrieved data, for example:
+            console.log(`Track Name: ${data.name}, Artist: ${data.artists[0].name}`);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
+
+// Call the function to get Spotify track information
+getSpotifyTrackInfo();
+
