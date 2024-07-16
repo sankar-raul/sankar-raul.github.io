52e94edde920550461f2cbcdfbadc81d2f9ac04b
sankar-raul <raulsankar99@gmail.com>
2024-07-16 15:24:06 +0530

    working on it

--- docs/script.js
+++ docs/script.js
@@ -1,87 +1,27 @@
-// Thats function really minimised 300 line of code 
-    function skeleton(rows,hex,parent,classes=[]) {
-        for (let i = 0;i<rows;i++) {
-            let row = document.createElement('div');
-    if (classes.length>0) {
-        classes.forEach(cls => row.classList.add(cls));
-    } else {
-        console.log("Classlist needed");
-        return 'Classlist must needed';
+// mouse pointer style
+var preloadCursorClicked
+window.addEventListener('load', () => {
+    preloadCursorClicked = new Image()
+    preloadCursorClicked.src = 'res/cursor-clicked.svg'
+})
+function clickFunc(element) {
+    element.target.style.cursor = `url(${preloadCursorClicked.src}) 9 9, auto`
+    setTimeout(() => {
+        element.target.style.cursor = "url('res/cursor.svg') 9 9, auto"
+    }, 100)
+}
+document.onclick = (element) => clickFunc(element)
+// mobike nav animation
+const mobileNavDetails = document.querySelector('.nav-mobile details')
+mobileNavDetails.addEventListener('toggle', () => {
+    const list = document.querySelector('.list-mobile')
+    const openState = () => {
+        list.style.animation = "fade 1s ease"
+        list.style.opacity = "1"
     }
-        for (let j = 0;j<hex;j++) {
-            let child = document.createElement('div');
-            child.classList.add('hexagon');
-            row.appendChild(child);
-            
-        }
-        parent.appendChild(row);
-        }
+    const notOpenState = () => {
+        list.style.animation = "none"
+        list.style.opacity = "0"
     }
-    
-    // adding hexagons to the intro bar
-     const parent = document.getElementById('intro1');
-     skeleton(11,18,parent,['row','over']);
-     
-     const parent2 = document.getElementById('placeit');
-     skeleton(4,18,parent2,['row','over','ov']);
-     
-     const parent3 = document.getElementById('container');
-     skeleton(24,18,parent3,['row']);
-     
-            const playmusic = document.getElementById("bgMusic");
-            var body1 = document.querySelector("body");
-            playmusic.src = "/res/bg.mp3";
-            body1.addEventListener("dblclick", function() {
-                if (!playmusic.paused) {
-                    playmusic.pause();
-                } else {
-                    playmusic.play();
-                }
-            });
-            var intro = document.getElementById("intro");
-            var intro1 = document.getElementById("intro1");
-            setTimeout(function() {
-                intro.style.display = "none";
-
-            }, 3500);
-
-            function redirect(name) {
-                const calculator = "https://sankar-raul.github.io/calculator_web/";
-                const twitter = "https://sankar-raul.github.io/twitter_clone";
-                const yt = "./youtube/";
-                const login = "./login/";
-                const clock = "https://sankar-raul.github.io/analog_clock/";
-                const simpleC = "./calculatorui/";
-                const music = "https://sankar-raul.github.io/zotify";
-                const sys = "https://userloginsystem.onrender.com/";
-                const check = name.innerText;
-                switch (check) {
-                    case "Calculator":
-                        window.open(calculator);
-                        break;
-                    case "TwitterX Clone":
-                        window.open(twitter);
-                        break;
-                    case "YouTube View Bot":
-                        window.open(yt);
-                        break;
-                    case "Login Form":
-                        window.open(login);
-                        break;
-                    case "Analog Clock":
-                        window.open(clock);
-                        break;
-                    case "Simple Calculator":
-                        window.open(simpleC);
-                        break;
-                    case "User Login System":
-                        window.open(sys);
-                    case "Music Player":
-                        window.open(music);
-                    default:
-                        console.log("Coming soon!");
-                }
-            }
-            function goto(url) {
-                window.open(url);
-            }
+    mobileNavDetails.open ? openState`` : notOpenState``
+})
